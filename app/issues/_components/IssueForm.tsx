d4808287f314f3b69ce979ab4bb54e8b118c9732
Dipak Giri <dipakgiri1200@gmail.com>
2024-01-27 18:45:01 +0545

    Build the edit issue page

---
+++ app/issues/_components/IssueForm.tsx
@@ -0,0 +1,78 @@
+'use client'
+
+import { createIssueSchema } from '@/app/validationSchema'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button, Callout, TextField } from '@radix-ui/themes'
+import axios from 'axios'
+import 'easymde/dist/easymde.min.css'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import SimpleMDE from 'react-simplemde-editor'
+
+import ErrorMessage from '@/app/components/ErrorMessage'
+import Spinner from '@/app/components/Spinner'
+import { z } from 'zod'
+import { Issue } from '@prisma/client'
+
+type IssueFormData = z.infer<typeof createIssueSchema>
+
+const IssueForm = ({ issue }: { issue?: Issue }) => {
+  const router = useRouter()
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueFormData>({
+    resolver: zodResolver(createIssueSchema),
+  })
+  const [error, setError] = useState('')
+  const [isSubmitting, setSubmitting] = useState(false)
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true)
+      await axios.post('/api/issues', data)
+      router.push('/issues')
+    } catch (err) {
+      setError('An unexpected error occur')
+    }
+  })
+
+  return (
+    <div className='max-w-xl '>
+      {error && (
+        <Callout.Root color='red' className='mb-4'>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form onSubmit={onSubmit} className=' space-y-3'>
+        <TextField.Root>
+          <TextField.Input
+            defaultValue={issue?.title}
+            placeholder='Title'
+            {...register('title')}
+          />
+        </TextField.Root>
+        {<ErrorMessage>{errors.title?.message}</ErrorMessage>}
+        <Controller
+          name='description'
+          defaultValue={issue?.description}
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder='description' {...field} />
+          )}
+        />
+        {<ErrorMessage>{errors.description?.message}</ErrorMessage>}
+
+        <Button disabled={isSubmitting}>
+          Submit New Issue
+          {isSubmitting && <Spinner />}
+        </Button>
+      </form>
+    </div>
+  )
+}
+
+export default IssueForm
