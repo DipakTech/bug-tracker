b7bb11d50687c834f1ff344c93faf6084799ef16
Dipak Giri <dipakgiri1200@gmail.com>
2024-01-28 10:38:37 +0545

    build an api for updating an issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { createIssueSchema } from '@/app/validationSchema'
+import { issueSchema } from '@/app/validationSchema'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Callout, TextField } from '@radix-ui/themes'
 import axios from 'axios'
@@ -15,7 +15,7 @@ import Spinner from '@/app/components/Spinner'
 import { z } from 'zod'
 import { Issue } from '@prisma/client'
 
-type IssueFormData = z.infer<typeof createIssueSchema>
+type IssueFormData = z.infer<typeof issueSchema>
 
 const IssueForm = ({ issue }: { issue?: Issue }) => {
   const router = useRouter()
@@ -25,7 +25,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
     handleSubmit,
     formState: { errors },
   } = useForm<IssueFormData>({
-    resolver: zodResolver(createIssueSchema),
+    resolver: zodResolver(issueSchema),
   })
   const [error, setError] = useState('')
   const [isSubmitting, setSubmitting] = useState(false)
